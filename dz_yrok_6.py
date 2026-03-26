a3bd6fb861532ce2d3c93a0270ff42eb010f9e43
Yarik0505 <greenyarik0505@gmail.com>
2026-03-26 13:30:11 +0200

    Create dz_yrok_6.py

---
+++ dz_yrok_6.py
@@ -0,0 +1,21 @@
+# 1
+for i in range(1, 16):
+    print(i)
+
+# 2
+n = int(input("Введіть число для таблиці множення: "))
+for i in range(1, 11):
+    print(n, "*", i, "=", n * i)
+
+# 3
+N = int(input("Введіть N: "))
+summa = 0
+for i in range(1, N + 1):
+    summa += i
+print("Сума:", summa)
+
+# 4
+clovo = input("Введіть слово: ")
+for c in clovo:
+    print(c)
+
