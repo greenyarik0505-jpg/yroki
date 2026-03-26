5f4962f32e41c53d7c007fe8cd0fe84320caf683
Yarik0505 <greenyarik0505@gmail.com>
2026-03-26 13:36:38 +0200

    Create dz_yrok_8.py

---
+++ dz_yrok_8.py
@@ -0,0 +1,21 @@
+# 1
+clovo = input("Введіть слово: ")
+print("Довжина:", len(clovo))
+print("Перший символ:", clovo[0])
+print("Останній символ:", clovo[-1])
+
+# 2
+clovo_2 = input("Введіть рядок: ")
+print("Чи входить а:", "а" in clovo_2.lower())
+
+# 3
+clovo_3 = input("Введіть слово (мінімум 6 літер): ")
+print("Перші 3:", clovo_3[:3])
+print("Останні 3:", clovo_3[-3:])
+
+# 4
+name = input("Твоє ім'я: ")
+age = input("Твій вік: ")
+print(f"Привіт, мене звати {name}, мені {age} років.")
+
+
